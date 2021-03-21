01fbcc8c44b7a936f3a4ae8bfcd883c460073111
Schrodingar <iv.aboyan@gmail.com>
2021-03-21 16:02:25 +0200

    Update

--- src/main/java/CashService/CashService.java
+++ src/main/java/CashService/CashService.java
@@ -1,14 +1,15 @@
 package CashService;
 
+import java.util.LinkedList;
 import java.util.concurrent.ConcurrentHashMap;
 
-public class CashService implements Cash{
+public class CashService implements Cash {
 
     private ConcurrentHashMap<String, Object> cache = new ConcurrentHashMap<>();
+    private LinkedList<String> keysList = new LinkedList();
 
     @Override
-    public Object get(String key)
-    {
+    public Object get(String key) {
         Object object = cache.get(key);
         if (object != null) {
             return object;
@@ -19,16 +20,26 @@ public class CashService implements Cash{
     @Override
     public Object put(String key, Object value) {
         Object ret;
-        synchronized(cache) {
+        synchronized (cache) {
             ret = cache.get(key);
             if (ret == null) {
+                keysList.add(key);
                 ret = cache.putIfAbsent(key, value);
                 if (ret == null) {
+                    eviction();
                     ret = value;
                 }
             }
         }
         return ret;
+    }
+
+    private void eviction() {
+        if (cache.size() > 100_000) {
+            String key = keysList.get(0);
+            keysList.remove(0);
+            cache.remove(key);
+        }
     }
 
 }
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -8,21 +8,14 @@ import java.util.stream.Stream;
 public class Main {
 
     public static void main(String[] args) {
-//        SimpleDateFormat formatForDateNow = new SimpleDateFormat("E yyyy.MM.dd 'и время' hh:mm:ss:SSS");
-//
-//        ArrayList<Integer> list = new ArrayList();
-//        int lengthList = 30_000_000;
-//        int searchElement = 20_152_123;
-//
-//        for (int i = 0; i < lengthList; i++) {
-//            list.add(i);
-//        }
-
 
         CashService cashService = new CashService();
-        cashService.put("first", new Integer(1));
-        cashService.put("second", new Integer(2));
-
+        for (int i = 0;  i < 1; i++) {
+            String key = Integer.toString(i);
+            Object value = i;
+            cashService.put(key, value);
+        }
+        System.out.println("");
     }
 
 }
